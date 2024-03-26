ff1071135e455ed96d43db9094e9e3b219b92a33
omerb <hannanorhn00@gmail.com>
2024-03-26 15:26:56 -0700

    playwright project created and initial test, click elemet, locators practices added

---
+++ tests/example.spec.ts
@@ -0,0 +1,65 @@
+import {test, expect} from '@playwright/test'
+
+test("simple basic test", async({page}) => {
+    //codes go here
+    await page.goto("https://www.example.com");
+
+    const pageTitle = await page.locator("h1");
+
+    await expect(pageTitle).toContainText("Example Domain");
+})
+
+test("Clicking on elements", async({page}) => {
+    await page.goto("http://zero.webappsecurity.com");
+
+    //locate by using id
+    await page.click("#signin_button");
+
+    //locate by using text
+    await page.click("text=Sign in");
+
+    //locate by using class and store in the variable
+    const errorMessage = await page.locator(".alert-error");
+
+    //do assertion
+    await expect(errorMessage).toContainText("Login and/or password are wrong.");
+})
+
+test("working with inputs", async ({page}) =>{
+    
+} )
+
+// test("selectors", async ({page}) => {
+//     //text
+//     await page.click("text=some text");
+
+//     //css 
+//     await page.click('button');
+
+//     //id
+//     await page.click('#id');
+
+//     //class
+//     await page.click('.class');
+
+//     //only visible css
+//     await page.click('.submit-button:visible');
+
+//     //combinations
+//     await page.click('#username .firt');
+
+//     //xpath
+//     await page.click('//button');
+// })
+
+/*
+Run test → npx playwright test
+-Headless mode to see the browser opening and performing testing on front of us → npx playwright test –headed
+
+Run in specific/all browser → 
+npx playwright test –browser=firefox
+npx playwright test –browser=all
+			npx playwright test –headed –browser=firefox
+
+Spesific test run → npx playwright test tests/example.spec.ts
+*/
