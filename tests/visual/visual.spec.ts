020b825f051bc7c7d4b06f2882aaad321071b6ab
omerb <hannanorhn00@gmail.com>
2024-04-24 10:53:27 -0700

    single and full page snapshot added into the visual regression

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -1,10 +1,27 @@
-import{test,expect} from "@playwright/test"
+import { test, expect } from '@playwright/test'
 
-test.describe("visual regression testing example",()=>{
+test.describe.only('visual regression testing example', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.example.com')
+  })
 
-test("full page snapshots",async({page})=>{
-    await page.goto("https://www.example.com")
-    expect(await page.screenshot()).toMatchSnapshot("homepage.png")
+  test('full page snapshots', async ({ page }) => {
+    expect(await page.screenshot()).toMatchSnapshot('homepage.png')
+  })
+
+  test('single element snapshot', async ({ page }) => {
+
+  const pageTitle = await page.$('h1');
+
+  // Check if pageTitle is not null before proceeding
+  if (pageTitle !== null) {
+    expect(await pageTitle.screenshot()).toMatchSnapshot("page-title.png");
+  } else {
+    // Handle the case where 'h1' element is not found
+    throw new Error("The 'h1' element was not found on the page.");
+  }
 })
+
+
 
 })
