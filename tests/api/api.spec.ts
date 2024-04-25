3b252723d9e54cb002a14095ad9c269327217be8
omerb <hannanorhn00@gmail.com>
2024-04-25 13:29:43 -0700

    simple api testing examples added

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,12 +1,28 @@
 import {test,expect} from '@playwright/test'
-import { request } from 'http'
 
-test.describe.parallel.only('Api Testing',()=>{
+test.describe.parallel('Api Testing',()=>{
     const baseURL='https://reqres.in/api'
-    test('Simple api test - Assert successful response status', async({request})=>{ //instead of page we have request in api testing
+    test('Simple api test - Assert successful response status', async({request})=>{
+         //instead of page we have request in api testing
         const response = await request.get(baseURL+'/users/2')
         expect(response.status()).toBe(200)
     })
 
+    test('Simple API Test - Assert Invalid Endpoint',async({request})=>{
+        const response = await request.get(baseURL+'/users/225252')
+        expect(response.status()).toBe(404)
+    })
+
+    test('Simple api test parsing response and do assertion', async({request})=>{
+        //instead of page we have request in api testing
+       const response = await request.get(baseURL+'/users/3')
+
+       //verify the successful status code first
+       expect(response.status()).toBe(200)
+
+       //parse the response as a JSON and store in the responseBody variable
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
+   })
 
 })
